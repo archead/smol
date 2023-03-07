9579ddddfb7402d5ed572439336ce6ae7ebf7da9
Archead <55419973+archead@users.noreply.github.com>
2023-03-07 03:48:15 +0000

    add smooth progress bar

--- smol.ps1
+++ smol.ps1
@@ -6,14 +6,24 @@ $bitrate = [int]((200000 - $dur * 128) / $dur)
 $bitrate = $bitrate.toString()+ "k"
 
 Write-Progress -Activity "Pass 1/2" -Status "0% Complete:" -PercentComplete 0
+
 ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 1 -an -f null NUL; `
-Write-Progress -Activity "Pass 2/2" -Status "50% Complete:" -PercentComplete 50
+
+for ($i = 1; $i -le 50; $i++ ) {
+    Write-Progress -Activity "Pass 2/2" -Status "$i% Complete:" -PercentComplete $i
+    Start-Sleep -Milliseconds 1
+}
+
 ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 2 -c:a aac -b:a 128k $filename
-Write-Progress -Activity "Done." -Status "100% Complete:" -PercentComplete 100
+
+for ($i = 50; $i -le 100; $i++ ) {
+    Write-Progress -Activity "Done." -Status "$i% Complete:" -PercentComplete $i
+    Start-Sleep -Milliseconds 1
+}
+
 Start-Sleep -Milliseconds 1000
 
 #ffmpeg -hide_banner -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -c:a aac -b:a 128k $filename
 rm .\ffmpeg2pass-0.log 
 
-Write-Output "Done."
 Write-Output "Video saved as: $filename" 
