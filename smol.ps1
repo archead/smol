31e9daf867cf21b44f9f4dee046621527431d012
Archead <55419973+archead@users.noreply.github.com>
2023-03-07 03:41:35 +0000

    fix bitrate algorithm and add prog. bar

--- smol.ps1
+++ smol.ps1
@@ -2,13 +2,18 @@ $dur = ffprobe $args[0] -show_entries format=duration -of compact=p=0:nk=1 -v 0
 $filename = $args[0] -replace '.\\',''
 $filename = "smol_" + $filename
 $dur = [int]$dur
-$bitrate = [int](200000 / $dur - 128)
+$bitrate = [int]((200000 - $dur * 128) / $dur)
 $bitrate = $bitrate.toString()+ "k"
-Write-Output "Making the video smol..."
-Write-Output "Please Wait."
+
+Write-Progress -Activity "Pass 1/2" -Status "0% Complete:" -PercentComplete 0
 ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 1 -an -f null NUL; `
-ffmpeg -hide_banner -loglevel error -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 2 -c:a aac -b:a 128k $filename
+Write-Progress -Activity "Pass 2/2" -Status "50% Complete:" -PercentComplete 50
+ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 2 -c:a aac -b:a 128k $filename
+Write-Progress -Activity "Done." -Status "100% Complete:" -PercentComplete 100
+Start-Sleep -Milliseconds 1000
+
+#ffmpeg -hide_banner -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -c:a aac -b:a 128k $filename
 rm .\ffmpeg2pass-0.log 
-#rm .\ffmpeg2pass-0.log.mbtree
+
 Write-Output "Done."
-Write-Output "Video saved as: " $filename
+Write-Output "Video saved as: $filename" 
