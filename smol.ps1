f3538adec909e1016589608b1bc9718ae07f636c
Archead <55419973+archead@users.noreply.github.com>
2023-03-08 03:50:09 +0000

    add even smoother progress bar

--- smol.ps1
+++ smol.ps1
@@ -1,29 +1,53 @@
+function Show-Progress 
+{
+    param (
+        $passnum
+    )
+
+    while (!(Test-Path "log.txt")) { Start-Sleep 1 }
+    $lastwrite = Get-Item log.txt
+    $offset, $progress = 0
+
+    While ($progress -lt 100) 
+    {
+        $currentwrite = Get-Item log.txt
+        if(($lastwrite.LastWriteTime) -lt ($currentwrite.LastWriteTime) -or (Get-Content log.txt -tail 1) -eq "progress=end")
+        {
+	        $frame = Get-Content log.txt | Select -Index $offset
+	        $currframe = [int]($frame.substring(6))
+	        $offset += 12
+	        $percent = [int]($currframe / $totalframes * 100)
+	        $progress = [int]$percent
+            Write-Progress -Activity "Pass $passnum/2" -Status "$progress% Complete:" -PercentComplete $progress
+            $lastwrite = $currentwrite
+        }
+    }
+}
+
+$totalframes = ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 $args[0]
+
 $dur = ffprobe $args[0] -show_entries format=duration -of compact=p=0:nk=1 -v 0
 $filename = $args[0] -replace '.\\',''
 $filename = "smol_" + $filename
 $dur = [int]$dur
 $bitrate = [int]((200000 - $dur * 128) / $dur)
-$bitrate = $bitrate.toString()+ "k"
+$bitrate = $bitrate.toString()+"k"
 
-Write-Progress -Activity "Pass 1/2" -Status "0% Complete:" -PercentComplete 0
 
-ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 1 -an -f null NUL; `
+if (Test-Path "ffmpeg2pass-0.log") { rm .\ffmpeg2pass-0.log }
+if (Test-Path "log.txt") { rm .\log.txt }
 
-for ($i = 1; $i -le 50; $i++ ) {
-    Write-Progress -Activity "Pass 2/2" -Status "$i% Complete:" -PercentComplete $i
-    Start-Sleep -Milliseconds 1
-}
+Start-Process -FilePath "ffmpeg" -ArgumentList "-progress log.txt -hide_banner -loglevel error -y -i", $args[0], "-vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 1 -an -f null NUL; ` " -NoNewWindow
+Show-Progress 1
 
-ffmpeg -hide_banner -loglevel error -y -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 2 -c:a aac -b:a 128k $filename
+rm .\log.txt
+Start-Process -FilePath "ffmpeg"  -ArgumentList "-progress log.txt -hide_banner -loglevel error -y -i", $args[0], "-vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -pass 2 -c:a aac -b:a 128k $filename" -NoNewWindow
+Show-Progress 2
 
-for ($i = 50; $i -le 100; $i++ ) {
-    Write-Progress -Activity "Done." -Status "$i% Complete:" -PercentComplete $i
-    Start-Sleep -Milliseconds 1
-}
-
-Start-Sleep -Milliseconds 1000
-
+# Might be used in the future, idk
 #ffmpeg -hide_banner -i $args[0] -vf scale=1280:720 -c:v libx264 -preset superfast -b:v $bitrate -c:a aac -b:a 128k $filename
+
 rm .\ffmpeg2pass-0.log 
+rm .\log.txt
 
 Write-Output "Video saved as: $filename" 
