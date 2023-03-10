5c566a149b79ce2598de0defbeef7ed79d6ebc22
Archead <55419973+archead@users.noreply.github.com>
2023-03-10 00:38:26 +0000

    automate installer

--- smolinstaller.ps1
+++ smolinstaller.ps1
@@ -1,3 +1,5 @@
+Write-Output "Installing smol..."
+Set-ExecutionPolicy Bypass -Scope Process -Force
 $profilescript = '
 function smol 
 {
@@ -51,11 +53,25 @@ function smol
 	Write-Output "Video saved as: $filename"
 }'
 
+
+
 if (!(Test-Path -Path $PROFILE)) {
-  New-Item -ItemType File -Path $PROFILE -Force
-  Add-Content $PROFILE $profilescript
+	Write-Output "Creating powershell profile..."
+	New-Item -ItemType File -Path $PROFILE -Force
+	Write-Output "adding smol to profile..."
+	Add-Content $PROFILE $profilescript
 }
 else {
+	Write-Output "adding smol to profile..."
 	Add-Content $PROFILE $profilescript
 }
 
+Write-Output "adding smol to context menu..."
+
+# Reg2CI (c) 2022 by Roger Zander
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\*\shell\smol") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\*\shell\smol" -force -ea SilentlyContinue };
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\*\shell\smol\command") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\*\shell\smol\command" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\*\shell\smol' -Name '(default)' -Value 'Compress with smol' -PropertyType String -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\*\shell\smol\command' -Name '(default)' -Value 'powershell.exe smol \"%1\"' -PropertyType String -Force -ea SilentlyContinue;
+
+Write-Output "smol installed successfully! You can now close this window."
